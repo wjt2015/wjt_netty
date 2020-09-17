f21405ca97286ceb81429e2ace08c16d0b7698cc
jintao9 <15236286736@163.com>
2020-09-17 18:15:49 +0800

    update

--- src/main/java/wjt/web/MyWebAppInitializer.java
+++ src/main/java/wjt/web/MyWebAppInitializer.java
@@ -1,11 +1,11 @@
 package wjt.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.MyAbstractAnnotationConfigDispatcherServletInitializer;
 import wjt.biz.BizConfig;
 
 @Slf4j
-public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MyWebAppInitializer extends MyAbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{BizConfig.class};
