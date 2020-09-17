abdb0d25744754f42a26ab544882fd7d1c4561ea
jintao9 <15236286736@163.com>
2020-09-17 18:21:04 +0800

    update

--- src/main/java/wjt/controller/AdminController.java
+++ src/main/java/wjt/controller/AdminController.java
@@ -1,10 +1,11 @@
-package wjt.web;
+package wjt.controller;
 
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import wjt.web.HttpResult;
 
 @Slf4j
 @RequestMapping(value = {"/admin"})
--- src/main/java/wjt/web/WebConfig.java
+++ src/main/java/wjt/web/WebConfig.java
@@ -1,11 +1,14 @@
 package wjt.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import wjt.controller.AdminController;
 
 @Slf4j
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackageClasses = {AdminController.class})
 public class WebConfig {
 }
