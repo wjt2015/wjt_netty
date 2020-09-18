b94498ea14fae202e6e162c901080a3e7596fc39
jintao9 <15236286736@163.com>
2020-09-19 03:19:36 +0800

    netty_1

--- src/main/java/wjt/netty/d/TelnetClient.java
+++ src/main/java/wjt/netty/d/TelnetClient.java
@@ -1,6 +1,7 @@
 package wjt.netty.d;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -27,6 +29,10 @@ public class TelnetClient {
     @Resource
     private TelnetClientInitializer telnetClientInitializer;
 
+    private List<String> lines = Lists.newArrayList("中标供应商对本次维修维保项目所更换的零件提供2年的保修服务，保修期自验收合格之日起计算",
+            "现非故意破坏性故障，由成交供应商负责免费维修确保设备正常运转，若设备损坏经双方确认后需要更换的",
+            "本项目维护保养的内容及基本要求均应遵守有关法规及标准，以及我校的各项管理制度。参与项目的所有人员需严格遵守国家及学校防疫工作要求，在校期间服从学校管理。");
+
     public void run() {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
@@ -34,33 +40,25 @@ public class TelnetClient {
             Channel ch = b.group(group)
                     .channel(NioSocketChannel.class)
                     .handler(telnetClientInitializer).connect(host, port).channel();
-            //read cmds from the stdin;
+
             ChannelFuture lastWriteFuture = null;
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-                while (true) {
-                    String line = reader.readLine();
-                    if (Strings.isNullOrEmpty(line)) {
-                        break;
-                    }
 
-                    final ByteBuf byteBuf = Unpooled.buffer(256);
-                    byteBuf.writeCharSequence(line + "\r\n", CharsetUtil.UTF_8);
-                    lastWriteFuture = ch.writeAndFlush(byteBuf);
-                    if (line.equalsIgnoreCase("bve")) {
-                        //wait until the server closes the connection;
-                        ch.closeFuture().sync();
-                        break;
-                    }
+            for (String line : lines) {
+                final ByteBuf byteBuf = Unpooled.buffer(256);
+                byteBuf.writeCharSequence(line + "\r\n", CharsetUtil.UTF_8);
+                lastWriteFuture = ch.writeAndFlush(byteBuf);
+                if (line.equalsIgnoreCase("bve")) {
+                    //wait until the server closes the connection;
+                    ch.closeFuture().sync();
+                    break;
                 }
-
-                if (lastWriteFuture != null) {
-                    //wait until all msgs are flushed before closing the channel;
-                    lastWriteFuture.sync();
-                }
-
-            } catch (Exception e) {
-                log.error("io error!", e);
             }
+
+            if (lastWriteFuture != null) {
+                //wait until all msgs are flushed before closing the channel;
+                lastWriteFuture.sync();
+            }
+
 
         } catch (Exception e) {
             log.error("client error!", e);
